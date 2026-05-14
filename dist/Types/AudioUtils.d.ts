5332c831a0f5b9f76cfe20d5d5af602004b87f13
Sam Bowden <91887909+Sam-Bowden@users.noreply.github.com>
2026-05-14 17:21:43 +0100

    fix: remove dist folders

--- dist/Types/AudioUtils.d.ts
+++
@@ -1,6 +0,0 @@
-export type AudioUtils = {
-    setAudioData(audioData: Uint8Array | Uint8Array[]): ThisType<AudioUtils>;
-    checkPreProcessVolume(): ThisType<AudioUtils>;
-    checkPostProcessVolume(): ThisType<AudioUtils>;
-    getAudioData(): Uint8Array;
-};
--- dist/Utils/State.d.ts
+++
@@ -1,7 +0,0 @@
-export type GateState = {
-    holdSamplesRemaining?: number;
-    attenuation: number;
-};
-export type DownwardCompressorState = {
-    ratio: number;
-};
